bca07f7b92f003882e8214d5d8308f6f301e90c1
周玉岩 <1774329934@qq.com>
2023-06-14 15:15:04 +0800

    Update TagAdapter.kt

--- library/src/main/java/com/magicgoop/tagsphere/TagAdapter.kt
+++ library/src/main/java/com/magicgoop/tagsphere/TagAdapter.kt
@@ -23,6 +23,30 @@ internal class TagAdapter {
      *   时候这个文章的算法
      * https://blog.csdn.net/fanfan_hongyun/article/details/91415726
      *
+     * https://zhuanlan.zhihu.com/p/19699319
+     *
+     * 本文采用的定义是：最大化任意两点间的最小距离。
+     * 其他可能的定义还有：球面上的同性电荷能量最低时的分布／离散估计球面积分时最合适的采样点。
+     *
+     *
+     * https://www.cnblogs.com/TappaT/p/15965985.html
+     * x2+y2+z2=1 这个公式不能扔掉
+     *
+     *
+     * https://zh.wikipedia.org/zh-hans/%E7%90%83%E5%BA%A7%E6%A8%99%E7%B3%BB
+     *
+     *
+     *
+     * 这个问题 todo：
+     * https://cloud.tencent.com/developer/ask/sof/106495/answer/102384708
+     *
+     * 在上面的找到下面的这个链接-----上面的连接就是翻译的这个内容而来的
+     *
+     * https://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere/26127012#26127012
+     *
+     *
+     *  单位球体的公式
+     *
      */
     private fun recalculate(i: Int, size: Int, tagItem: TagItem) {
         val offset: Float = 2f / size
